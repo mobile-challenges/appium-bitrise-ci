99de41a04557a582f500fbccdf60dc43cfe4b103
Moataz Nabil <moataz.eldebsy@moataznabil-mbp.localdomain>
2021-08-25 18:12:32 +0200

    adding applitools integration

--- src/test/java/FirstAndroidTest.java
+++ src/test/java/FirstAndroidTest.java
@@ -1,3 +1,5 @@
+import com.applitools.eyes.BatchInfo;
+import com.applitools.eyes.appium.Eyes;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
@@ -14,18 +16,28 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+@SuppressWarnings("rawtypes")
 public class FirstAndroidTest {
 
     AppiumDriver driver;
-    private static By addPlant = By.id("add_plant");
-    private static By add = By.id("fab");
+    private static final By addPlant = By.id("add_plant");
+    private static final By add = By.id("fab");
+    Eyes eyes = new Eyes();
 
     @BeforeTest
     public void setUp() throws MalformedURLException {
+        String apiKey = System.getenv("APPLITOOLS_API_KEY");
+        eyes.setApiKey(apiKey);
+        String batchName = null;
+        String batchId   = System.getenv("APPLITOOLS_BATCH_ID");
+        BatchInfo batchInfo = new BatchInfo(batchName);
+        batchInfo.setId(batchId);
+        eyes.setBatch(batchInfo);
+        eyes.setForceFullPageScreenshot(false);
+
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
-        caps.setCapability("automationName", "UiAutomator2");
-        caps.setCapability("platformVersion", "9");
+        caps.setCapability("platformVersion", "10");
         caps.setCapability("deviceName", "Android Emulator");
         caps.setCapability("app", System.getenv("BITRISE_APK_PATH"));
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
@@ -34,34 +46,37 @@ public class FirstAndroidTest {
     @Test
     public void add_plant_test() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
-
+        eyes.open(driver, "Sunflower", "Add My Plant");
         MobileElement plantList = (MobileElement) driver.findElementByAccessibilityId("Plant list");
         MobileElement myGarden = (MobileElement) driver.findElementByAccessibilityId("My garden");
-
         wait.until(ExpectedConditions.elementToBeClickable(plantList)).click();
+        eyes.checkWindow("Plant list",false);
         wait.until(ExpectedConditions.elementToBeClickable(myGarden)).click();
-
+        eyes.checkWindow("My Empty Garden",false);
         wait.until(ExpectedConditions.presenceOfElementLocated(addPlant)).click();
-        List <MobileElement> listElements = driver.findElements(By.id("plant_item_title"));
-        for(MobileElement el : listElements){
-            if(el.getText().equalsIgnoreCase("Avocado")){
-               el.click();
-               break;
+        List<MobileElement> listElements = driver.findElements(By.id("plant_item_title"));
+        for (MobileElement el : listElements) {
+            if (el.getText().equalsIgnoreCase("Avocado")) {
+                el.click();
+                break;
             }
         }
         wait.until(ExpectedConditions.presenceOfElementLocated(add)).click();
         driver.navigate().back();
         driver.findElementByAccessibilityId("My garden").click();
-        By plant_name =  By.id("plant_name");
+        By plant_name = By.id("plant_name");
         wait.until(ExpectedConditions.presenceOfElementLocated(plant_name));
+        eyes.checkWindow("Avocado",false);
         Assert.assertTrue(driver.findElement(plant_name).getAttribute("text")
                 .equalsIgnoreCase("Avocado"));
+        eyes.close();
     }
 
     @AfterTest
     public void tearDown() {
         if (null != driver) {
             driver.quit();
+            eyes.abortIfNotClosed();
         }
     }
 }
