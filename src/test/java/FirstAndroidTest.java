88fe651c93478e81fccb67aae36532a646cd3f28
mobile-challenges <87436973+mobile-challenges@users.noreply.github.com>
2021-10-25 13:49:53 -0300

    Update FirstAndroidTest.java

--- src/test/java/FirstAndroidTest.java
+++ src/test/java/FirstAndroidTest.java
@@ -27,7 +27,7 @@ public class FirstAndroidTest {
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("platformVersion", "9");
         caps.setCapability("deviceName", "Android Emulator");
-        caps.setCapability("app", System.getenv("BITRISE_APK_PATH"));
+        caps.setCapability("app", "app-debug.apk);
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
     }
 
